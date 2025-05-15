f94e59de6397326ab52452ca75a733e6e0a0b8e2
alex <alex.sanders@ukr.net>
2025-05-15 20:56:03 +0300

    Update AdminController.php

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -12,45 +12,45 @@ class AdminController extends Controller
 {
     public function index()
     {
-        $todayOrders = Order::whereDay('created_at', '', Carbon::today())->get();
-        $yesterdayOrders = Order::whereDay('created_at', '', Carbon::yesterday())->get();
-        $monthOrders = Order::whereMonth('created_at', '', Carbon::now()->month)->get();
-        $yearOrders = Order::whereYear('created_at', '', Carbon::now()->year)->get();
+        $todayOrders = Order::whereDay( 'created_at', Carbon::today() )->get();
+        $yesterdayOrders = Order::whereDay( 'created_at', Carbon::yesterday() )->get();
+        $monthOrders = Order::whereMonth( 'created_at', Carbon::now()->month )->get();
+        $yearOrders = Order::whereYear( 'created_at', Carbon::now()->year )->get();
 
-        return view('admin.dashboard')
-            ->with([
+        return view( 'admin.dashboard' )
+            ->with( [
                 'todayOrders' => $todayOrders,
                 'yesterdayOrders' => $yesterdayOrders,
                 'monthOrders' => $monthOrders,
                 'yearOrders' => $yearOrders,
-            ]);
+            ] );
     }
 
     /** Display the login form */
     public function login()
     {
-        if (! auth()->guard('admin')->check()) {
-            return view('login');
+        if ( !auth()->guard( 'admin' )->check() ) {
+            return view( 'login' );
         }
 
-        return redirect()->route('admin.index');
+        return redirect()->route( 'admin.index' );
     }
 
     /** Login the admin */
-    public function auth(AuthAdminRequest $request)
+    public function auth( AuthAdminRequest $request )
     {
-        if ($request->validated()) {
-            if (auth()->guard('admin')->attempt([
+        if ( $request->validated() ) {
+            if ( auth()->guard( 'admin' )->attempt( [
                 'email' => $request->email,
                 'password' => $request->password,
-            ])) {
+            ] ) ) {
                 $request->session()->regenerate();
-                return redirect()->route('admin.index');
+                return redirect()->route( 'admin.index' );
             } else {
-                return redirect()->route('admin.login')
-                    ->with([
+                return redirect()->route( 'admin.login' )
+                    ->with( [
                         'error' => 'These credentials do not match any of our records',
-                    ]);
+                    ] );
             }
         }
     }
@@ -58,7 +58,7 @@ class AdminController extends Controller
     /** Logout the admin */
     public function logout()
     {
-        auth()->guard('admin')->logout();
-        return redirect()->route('admin.login');
+        auth()->guard( 'admin' )->logout();
+        return redirect()->route( 'admin.login' );
     }
 }
